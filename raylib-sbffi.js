a00cbb8cfa802d6ae45009e4bebe282bfd171ed9
David Konsumer <dkonsumer@gummicube.com>
2022-03-05 20:48:02 -0800

    fill in sbffi demo. still fils, but it should work

--- raylib-sbffi.js
+++ raylib-sbffi.js
@@ -4,31 +4,7 @@ import { getNativeFunction, getBufferPointer } from 'sbffi'
 const str = value => getBufferPointer(Buffer.from(value))
 
 // these are RGBA hex numbers
-const LIGHTGRAY = 0xc8c8c8ff
-const GRAY = 0x828282ff
-const DARKGRAY = 0x505050ff
-const YELLOW = 0xfdf900ff
-const GOLD = 0xffcb00ff
-const ORANGE = 0xffa100ff
-const PINK = 0xff6dc2ff
-const RED = 0xe62937ff
-const MAROON = 0xbe2137ff
-const GREEN = 0x00e430ff
-const LIME = 0x009e2fff
-const DARKGREEN = 0x00752cff
-const SKYBLUE = 0x66bfffff
-const BLUE = 0x0079f1ff
-const DARKBLUE = 0x0052acff
-const PURPLE = 0xc87affff
-const VIOLET = 0x873cbeff
-const DARKPURPLE = 0x701f7eff
-const BEIGE = 0xd3b083ff
-const BROWN = 0x7f6a4fff
-const DARKBROWN = 0x4c3f2fff
 const WHITE = 0xffffffff
-const BLACK = 0x000000ff
-const BLANK = 0x00000000
-const MAGENTA = 0xff00ffff
 const RAYWHITE = 0xf5f5f5ff
 
 const libPath = 'libraylib.dylib' 
@@ -42,17 +18,31 @@ export const DrawFPS = getNativeFunction(libPath, 'DrawFPS', 'void', ['int', 'in
 export const CloseWindow = getNativeFunction(libPath, 'CloseWindow', 'void', [])
 export const GetFPS = getNativeFunction(libPath, 'GetFPS', 'int', [])
 export const LoadTexture = getNativeFunction(libPath, 'LoadTexture', 'pointer', ['pointer'])
+export const DrawTexture = getNativeFunction(libPath, 'DrawTexture', 'void', ['pointer', 'int', 'int', 'unsigned int'])
 
 InitWindow(800, 450, str('raylib [textures] example - bunnymark'))
 
 // not sure why this crashes
 const texBunny = LoadTexture(str('resources/wabbit_alpha.png'))
 
+const bunnies = []
+
 while(!WindowShouldClose()) {
+  if (GetFPS() > 60) {
+    for (let i = 0; i < 10; i++) {
+      bunnies.push([GetRandomValue(0, 800), GetRandomValue(0, 450)])
+    }
+  }
+
   BeginDrawing()
   ClearBackground(RAYWHITE)
+  bunnies.forEach(([x, y]) => {
+    DrawTexture(texBunny, x, y, WHITE)
+  })
   DrawFPS(10, 10)
   EndDrawing()
 }
+
+console.log(bunnies.length, 'bunnies')
 
 CloseWindow()
